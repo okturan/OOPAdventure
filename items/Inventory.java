448a5cf5d4777501641bc51a449c8f609a616696
okturan <okanert@gmail.com>
2024-06-06 06:40:07 +0300

    minor fixes

--- items/Inventory.java
+++ items/Inventory.java
@@ -11,37 +11,38 @@ public class Inventory {
         this.items = ItemFactory.createStarterItems();
     }
 
+    public static void printBetterItemWarning() {
+        System.out.println("\nYou already have this item or a better item.");
+    }
+
     public void addItem(Item item) {
         boolean shouldAdd = true;
 
         for (int i = 0; i < items.size(); i++) {
             Item existingItem = items.get(i);
 
-            // Check if the existing item is of the same class or superclass as the new item
+            // If the new item is not repeatable and the existing item is of the same class or superclass
             if (item != null && !item.isRepeatable() && existingItem.getClass().getSuperclass().equals(item.getClass().getSuperclass())) {
+                // Compare the values of the existing item and the new item
                 if (existingItem.getValue() >= item.getValue()) {
+                    // If the existing item has an equal or greater value, do not add the new item
                     System.out.print("Enemy drops a: " + item.getClass().getSimpleName());
                     printBetterItemWarning();
                     shouldAdd = false;
-                    break;
                 } else {
-                    // Remove the existing item from the same class since the new item has a higher value
+                    // Remove the existing item if the new item has a higher value
                     items.remove(i);
-                    break;
                 }
+                break;
             }
         }
 
+        // Add the new item to the inventory if it passes the checks
         if (shouldAdd) {
             items.add(item);
-            System.out.println("\uD83D\uDCE6 " + item + " added to the inventory.");
+            System.out.println("\n\uD83D\uDCE6 " + item + " added to the inventory.");
         }
     }
-
-    public static void printBetterItemWarning() {
-        System.out.println("\nYou already have this item or a better item.");
-    }
-
 
     public void listItems() {
         if (items.isEmpty()) {
